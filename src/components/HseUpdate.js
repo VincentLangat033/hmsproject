381604aad42a859b04c72735168530c67e99da8a
Vincent Langat <kimutailangat033@gmail.com>
2022-07-28 19:54:33 +0300

    will clear white spaces and ensure display name and image

--- src/components/HseUpdate.js
+++ src/components/HseUpdate.js
@@ -1,13 +1,10 @@
 import React,{useState,useEffect} from 'react'
-
 function HseUpdate() {
     const [houseItems, setHouseItems] = useState([]);
     useEffect(()=>{
         fetch('https://hms-server1235.herokuapp.com/houses')
         .then((res)=>res.json())
         .then((data)=>setHouseItems(data))
-        
-
     })
     const houseItemsList = houseItems.map((house)=>(
         <div className='mainhouseItems'>
@@ -15,31 +12,21 @@ function HseUpdate() {
                 <div className='imageParentDiv'>
                 <div className='imageOneList'>
                       <img src= {house.image1} alt="Image house" className='imageList'></img>
-
                 </div>
                 <div className='imageTwoList'>
                 <img src= {house.image2} alt="Image house" className=''></img>
                 <img src= {house.image2} alt="Image house" className=''></img>
                 <img src= {house.image2} alt="Image house" className=''></img>
                 <img src= {house.image2} alt="Image house" className=''></img>
-
-                </div>
-            
-                       
-               
+                </div>  
             </div>  
                <p> {house.name}</p>
         </div>
         </div> 
-        
-
     ));
   return (
      <div> 
          {houseItemsList}
-         
-       
-
     </div>
   )
 }
