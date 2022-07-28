66636eb8130d1c36d2a840e0e41643dedd3e23cc
Vincent Langat <kimutailangat033@gmail.com>
2022-07-28 19:56:02 +0300

    clear white space and delete commented text

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./css/login.css"
-
 function Login() {
   return (
     <div className='login'>
@@ -8,7 +7,6 @@ function Login() {
        <div className='loginLeft'>
          <h3 className='loginLogo'> HMS</h3>
          <span className='loginDesc'> Log in to begin</span>
-
        </div>
        <div className='loginRight'>
          <div className='loginBox'>
@@ -17,15 +15,9 @@ function Login() {
            <button className="loginButtton">Log In</button>
            <span className="loginForgot">Forgot Password? </span>
            <button className="loginRegisterButton">Create a New Account</button>
-         
-         
          </div>
-
        </div>
-      
-
      </div>
-
     </div>
   )
 }
