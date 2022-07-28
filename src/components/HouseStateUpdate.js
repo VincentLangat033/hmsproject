4e4b35329141ba720b6ab127483f3a45a4478668
Vincent Langat <kimutailangat033@gmail.com>
2022-07-28 19:52:10 +0300

    will check state of house if reserved , use ternary condition

--- src/components/HouseStateUpdate.js
+++ src/components/HouseStateUpdate.js
@@ -9,8 +9,6 @@ function HouseStateUpdate({house}) {
       setMarkedAsRead((markAsRead) => !markAsRead);
     }
      const isSetAsRserved= setAsReserved ? "Set as Reserved" : "Set as Unreserved"
-    
-
   return (
     <div>
         <button  onClick={handleMarkedAsRead} className='imageButton2 imageButton3'>{isSetAsRserved}</button>
