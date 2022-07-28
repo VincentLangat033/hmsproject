81912c14f7033c0623769508e25e89626206382d
Vincent Langat <kimutailangat033@gmail.com>
2022-07-28 23:48:10 +0300

    add styling to Navbar components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,14 @@
 import React from 'react'
 import './css/header.css'
 import { NavLink } from 'react-router-dom'
-
 function NavBar() {
   return (
-    
       <div className='headerContent'>
-        {/* <h1> NavBar</h1> */}
         <NavLink to='/houses'>
         <a href='#'> <button className='navHms'> HMS</button> </a>
         </NavLink>
         <NavLink to="/houses">
         <button className='navEdit'>Home</button>
-
         </NavLink>
         <NavLink to="/about">
         <button className='navEdit'>About</button>
@@ -24,12 +20,7 @@ function NavBar() {
         <button className='navEdit'>Log In</button>
         </NavLink>
         
-        
-    
-        
-        
-        
-       
+ 
     </div>
   )
 }
