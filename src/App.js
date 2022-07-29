6f853bf622f5f4e64744bce04016b61a80e57fa9
Vincent Langat <kimutailangat033@gmail.com>
2022-07-29 13:26:42 +0300

    this commit will set up final routes path clear whitespaces and remove  components which wont be consumed from the parent node

--- src/App.js
+++ src/App.js
@@ -2,14 +2,10 @@ import NavBar from "./components/NavBar";
 import Login from "./components/Login";
 import Register from "./components/Register"
 import SideNavBar from "./components/SideNavBar";
-import House from "./components/House";
 import '../src/components/css/house.css'
-import HouseItems from "./components/HouseItems";
-
 import { Route, Routes } from "react-router-dom";
 import HouseProducts from "./components/HouseProducts";
 import HouseForm from "./components/HouseForm";
-import HousePostDetails from "./components/HousePostDetails";
 import HseUpdate from "./components/HseUpdate";
 
 function App() {
@@ -26,7 +22,7 @@ function App() {
         <div className='midContentBody'>
         
           <Routes>
-       <Route exact path="/houses" element={<HouseProducts />}/>
+       <Route exact path="/" element={<HouseProducts />}/>
        <Route exact path="/houseform" element ={<HouseForm />} />
        <Route exact path="/updatehousename" element ={<HseUpdate />} />
        <Route exact path="/register" element ={<Register />} />
